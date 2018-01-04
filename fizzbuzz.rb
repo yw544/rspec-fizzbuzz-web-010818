10549aef059ac75eb6576fd201896700fb943a4c
York Wang <yw544@nyu.edu>
2018-01-04 14:04:21 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz(numnber)
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  elsif number % 5 == 0
+    return "Buzz"
+  elsif number % 3 == 0
+    return "Fizz"
+
+  end
+end
